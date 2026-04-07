e62cd048b64900a6972fe9ecc7fe8b5d72b27415
Joynal <joynalabdul2004@gmail.com>
2026-04-07 16:30:36 +0000

    added humidity readings, created temperature table, stored temperature table(unconfirmed), established connection psql, installed psql, created user granted privileges, solved minor bugs such as syntax, esp32 script modified, reversed to ip connection instead of pihole local domain dns

--- Backend/main.py
+++ Backend/main.py
@@ -8,40 +8,61 @@ import psycopg2
 import json
 import os
 
-con = psycopg2.connect(
-    host = "localhost",
-    database="db_name",
-    user="postgres",
-    password="pas"
-)
-
-cur = con.cursor()
-
 app = FastAPI()
 
 app.mount("/home", StaticFiles(directory="home", html=True), name="components")
 
-current_temperature = ""
+
+def get_connection():
+
+    return psycopg2.connect(
+        host = "localhost",
+        database="tempdb",#
+        user="lan_serv",#
+        password="esptemp32"#
+    )
 
 @app.get("/")
 def root():
     return RedirectResponse("/home")
 
-#This updates the current temperature when the esp sends it
-@app.get("/set")
-def set_message(msg: str):
+
+@app.on_event("shutdown") # when we close server it will first close the database connection and then shutdown
+def shutdown_event():
+    print("Server is shutting down...")
+
+
+#This updates the current temperature when the esp sends it + stores it in database
+@app.get("/set") # format of url is: http://IPAddress/set?temp=25&hum=60
+def set_message(temp: str, hum: str):
+
     global current_temperature
-    current_temperature = msg
-    return {"stored": current_temperature}
+    current_temperature = temp
+
+    global current_humidity
+    current_humidity = hum
+
+    try:
+        con = get_connection()
+        cur = con.cursor()
+
+        cur.execute(
+            "INSERT INTO sensor_data (temperature, humidity) VALUES (%s, %s);",
+            (current_temperature, current_humidity)
+        )
+
+        con.commit()
+        cur.close()
+        con.close()
+
+    except Exception as e:
+        return {"error": str(e)}
+
+    return {"stored": current_temperature, "humidity": current_humidity}
+
+
 
 #send temperature reading to front end JS script
 @app.get("/temperature")
 def pass_temperature():
-    if (current_temperature == ""):
-        current_temperature == "12"
-        return {"temperature" : current_temperature}
-    else:
-        return {"temperature" : current_temperature}
-    
-def store_data(x):
-    #When data is received store in database
+    return {"temperature" : current_temperature}
